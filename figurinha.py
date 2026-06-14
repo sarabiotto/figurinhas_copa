2fee70d1c3958770b5cb5d437141094f0abb9dbd
sarabiotto <sarabiotto@gmail.com>
2026-06-14 11:59:11 -0300

    Update figurinha.py

--- figurinha.py
+++ figurinha.py
@@ -2,13 +2,13 @@ class Figurinha:
     """Representa uma figurinha da Copa do Mundo."""
 
     def __init__(self, id, nome, pais, posicao, raridade):
-        self.id = id              # número da figurinha no álbum
-        self.nome = nome          # nome do jogador
-        self.pais = pais          # seleção (ex: Brasil, Argentina)
-        self.posicao = posicao    # posição em campo (ex: Atacante)
-        self.raridade = raridade  # comum, prata, ouro, lendária...
+        self.id = id              
+        self.nome = nome         
+        self.pais = pais          
+        self.posicao = posicao    
+        self.raridade = raridade  
 
     def __str__(self):
-        # Define como a figurinha aparece quando damos print()
+        
         return (f"#{self.id} | {self.nome} ({self.pais}) "
                 f"- {self.posicao} - {self.raridade}")
