8532324d09b47e0311996270c40ae4b8a08c840f
sarabiotto <sarabiotto@gmail.com>
2026-06-14 11:52:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,18 @@
 from figurinha import Figurinha
+from album import Album
 
-f = Figurinha(1, "Vinícius Jr.", "Brasil", "Atacante", "ouro")
-print(f)
+album = Album()
+
+album.adicionar(Figurinha(1, "Vinícius Jr.", "Brasil", "Atacante", "ouro"))
+album.adicionar(Figurinha(2, "Messi", "Argentina", "Atacante", "lendária"))
+album.adicionar(Figurinha(3, "Mbappé", "França", "Atacante", "ouro"))
+
+print("--- Álbum completo ---")
+album.listar()
+
+print("\n--- Buscando a figurinha 2 ---")
+print(album.buscar(2))
+
+print("\n--- Tentando adicionar a 2 de novo ---")
+repetida = album.adicionar(Figurinha(2, "Messi", "Argentina", "Atacante", "lendária"))
+print("Foi adicionada?", repetida)   # deve dar False
